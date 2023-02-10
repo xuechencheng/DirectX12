65a86ba52ed6c1bbebe6961150de90eada388575
xuecc <xuecc@adyorha.com>
2023-02-10 19:58:42 +0800

    1

--- Codes/Common/GameTimer.cpp
+++ Codes/Common/GameTimer.cpp
@@ -5,17 +5,16 @@
 #include <windows.h>
 #include "GameTimer.h"
 
-GameTimer::GameTimer()
-: mSecondsPerCount(0.0), mDeltaTime(-1.0), mBaseTime(0), 
-  mPausedTime(0), mPrevTime(0), mCurrTime(0), mStopped(false)
+GameTimer::GameTimer() : mSecondsPerCount(0.0), mDeltaTime(-1.0), mBaseTime(0), mPausedTime(0), mPrevTime(0), mCurrTime(0), mStopped(false)
 {
 	__int64 countsPerSec;
 	QueryPerformanceFrequency((LARGE_INTEGER*)&countsPerSec);
 	mSecondsPerCount = 1.0 / (double)countsPerSec;
 }
 
-// Returns the total time elapsed since Reset() was called, NOT counting any
-// time when the clock is stopped.
+/// <summary>
+/// Done
+/// </summary>
 float GameTimer::TotalTime()const
 {
 	// If we are stopped, do not count the time that has passed since we stopped.
@@ -26,12 +25,10 @@ float GameTimer::TotalTime()const
 	//                     |<--paused time-->|
 	// ----*---------------*-----------------*------------*------------*------> time
 	//  mBaseTime       mStopTime        startTime     mStopTime    mCurrTime
-
 	if( mStopped )
 	{
-		return (float)(((mStopTime - mPausedTime)-mBaseTime)*mSecondsPerCount);
+		return (float)(((mStopTime - mPausedTime)-mBaseTime) * mSecondsPerCount);
 	}
-
 	// The distance mCurrTime - mBaseTime includes paused time,
 	// which we do not want to count.  To correct this, we can subtract 
 	// the paused time from mCurrTime:  
@@ -41,18 +38,21 @@ float GameTimer::TotalTime()const
 	//                     |<--paused time-->|
 	// ----*---------------*-----------------*------------*------> time
 	//  mBaseTime       mStopTime        startTime     mCurrTime
-	
 	else
 	{
-		return (float)(((mCurrTime-mPausedTime)-mBaseTime)*mSecondsPerCount);
+		return (float)(((mCurrTime-mPausedTime)- mBaseTime) * mSecondsPerCount);
 	}
 }
-
+/// <summary>
+/// Done
+/// </summary>
 float GameTimer::DeltaTime()const
 {
 	return (float)mDeltaTime;
 }
-
+/// <summary>
+/// Done
+/// </summary>
 void GameTimer::Reset()
 {
 	__int64 currTime;
@@ -64,40 +64,44 @@ void GameTimer::Reset()
 	mStopped  = false;
 }
 
+/// <summary>
+/// Done
+/// </summary>
 void GameTimer::Start()
 {
 	__int64 startTime;
 	QueryPerformanceCounter((LARGE_INTEGER*)&startTime);
-
 
 	// Accumulate the time elapsed between stop and start pairs.
 	//
 	//                     |<-------d------->|
 	// ----*---------------*-----------------*------------> time
 	//  mBaseTime       mStopTime        startTime     
-
 	if( mStopped )
 	{
 		mPausedTime += (startTime - mStopTime);	
-
 		mPrevTime = startTime;
 		mStopTime = 0;
 		mStopped  = false;
 	}
 }
 
+/// <summary>
+/// Done
+/// </summary>
 void GameTimer::Stop()
 {
 	if( !mStopped )
 	{
 		__int64 currTime;
 		QueryPerformanceCounter((LARGE_INTEGER*)&currTime);
-
 		mStopTime = currTime;
 		mStopped  = true;
 	}
 }
-
+/// <summary>
+/// Done
+/// </summary>
 void GameTimer::Tick()
 {
 	if( mStopped )
@@ -105,17 +109,13 @@ void GameTimer::Tick()
 		mDeltaTime = 0.0;
 		return;
 	}
-
 	__int64 currTime;
 	QueryPerformanceCounter((LARGE_INTEGER*)&currTime);
 	mCurrTime = currTime;
-
 	// Time difference between this frame and the previous.
 	mDeltaTime = (mCurrTime - mPrevTime)*mSecondsPerCount;
-
 	// Prepare for next frame.
 	mPrevTime = mCurrTime;
-
 	// Force nonnegative.  The DXSDK's CDXUTTimer mentions that if the 
 	// processor goes into a power save mode or we get shuffled to another
 	// processor, then mDeltaTime can be negative.
--- Codes/Common/d3dApp.cpp
+++ Codes/Common/d3dApp.cpp
@@ -23,8 +23,7 @@ D3DApp* D3DApp::GetApp()
     return mApp;
 }
 
-D3DApp::D3DApp(HINSTANCE hInstance)
-:	mhAppInst(hInstance)
+D3DApp::D3DApp(HINSTANCE hInstance) : mhAppInst(hInstance)
 {
     // Only one D3DApp can be constructed.
     assert(mApp == nullptr);
@@ -62,19 +61,18 @@ void D3DApp::Set4xMsaaState(bool value)
     if(m4xMsaaState != value)
     {
         m4xMsaaState = value;
-
         // Recreate the swapchain and buffers with new multisample settings.
         CreateSwapChain();
         OnResize();
     }
 }
-
+/// <summary>
+/// Done
+/// </summary>
 int D3DApp::Run()
 {
 	MSG msg = {0};
- 
 	mTimer.Reset();
-
 	while(msg.message != WM_QUIT)
 	{
 		// If there are Window messages then process them.
@@ -87,7 +85,6 @@ int D3DApp::Run()
 		else
         {	
 			mTimer.Tick();
-
 			if( !mAppPaused )
 			{
 				CalculateFrameStats();
@@ -100,24 +97,25 @@ int D3DApp::Run()
 			}
         }
     }
-
 	return (int)msg.wParam;
 }
-
+/// <summary>
+/// Done
+/// </summary>
+/// <returns></returns>
 bool D3DApp::Initialize()
 {
 	if(!InitMainWindow())
 		return false;
-
 	if(!InitDirect3D())
 		return false;
-
     // Do the initial resize code.
     OnResize();
-
 	return true;
 }
- 
+/// <summary>
+/// 创建应用程序所需的描述符堆
+/// </summary>
 void D3DApp::CreateRtvAndDsvDescriptorHeaps()
 {
     D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc;
@@ -125,44 +123,34 @@ void D3DApp::CreateRtvAndDsvDescriptorHeaps()
     rtvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
     rtvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
 	rtvHeapDesc.NodeMask = 0;
-    ThrowIfFailed(md3dDevice->CreateDescriptorHeap(
-        &rtvHeapDesc, IID_PPV_ARGS(mRtvHeap.GetAddressOf())));
-
+    ThrowIfFailed(md3dDevice->CreateDescriptorHeap( &rtvHeapDesc, IID_PPV_ARGS(mRtvHeap.GetAddressOf())));
 
     D3D12_DESCRIPTOR_HEAP_DESC dsvHeapDesc;
     dsvHeapDesc.NumDescriptors = 1;
     dsvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_DSV;
     dsvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
 	dsvHeapDesc.NodeMask = 0;
-    ThrowIfFailed(md3dDevice->CreateDescriptorHeap(
-        &dsvHeapDesc, IID_PPV_ARGS(mDsvHeap.GetAddressOf())));
+    ThrowIfFailed(md3dDevice->CreateDescriptorHeap( &dsvHeapDesc, IID_PPV_ARGS(mDsvHeap.GetAddressOf())));
 }
-
+/// <summary>
+/// Done
+/// </summary>
 void D3DApp::OnResize()
 {
 	assert(md3dDevice);
 	assert(mSwapChain);
     assert(mDirectCmdListAlloc);
-
 	// Flush before changing any resources.
 	FlushCommandQueue();
-
     ThrowIfFailed(mCommandList->Reset(mDirectCmdListAlloc.Get(), nullptr));
-
 	// Release the previous resources we will be recreating.
 	for (int i = 0; i < SwapChainBufferCount; ++i)
 		mSwapChainBuffer[i].Reset();
     mDepthStencilBuffer.Reset();
-	
 	// Resize the swap chain.
-    ThrowIfFailed(mSwapChain->ResizeBuffers(
-		SwapChainBufferCount, 
-		mClientWidth, mClientHeight, 
-		mBackBufferFormat, 
-		DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH));
-
+	//7，调整后台缓冲区的大小，并为它创建渲染目标视图
+    ThrowIfFailed(mSwapChain->ResizeBuffers( SwapChainBufferCount, mClientWidth, mClientHeight, mBackBufferFormat, DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH));
 	mCurrBackBuffer = 0;
- 
 	CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHeapHandle(mRtvHeap->GetCPUDescriptorHandleForHeapStart());
 	for (UINT i = 0; i < SwapChainBufferCount; i++)
 	{
@@ -170,7 +158,7 @@ void D3DApp::OnResize()
 		md3dDevice->CreateRenderTargetView(mSwapChainBuffer[i].Get(), nullptr, rtvHeapHandle);
 		rtvHeapHandle.Offset(1, mRtvDescriptorSize);
 	}
-
+	//8，创建深度/模板缓冲区及与之关联的深度/模板视图
     // Create the depth/stencil buffer and view.
     D3D12_RESOURCE_DESC depthStencilDesc;
     depthStencilDesc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE2D;
@@ -179,31 +167,21 @@ void D3DApp::OnResize()
     depthStencilDesc.Height = mClientHeight;
     depthStencilDesc.DepthOrArraySize = 1;
     depthStencilDesc.MipLevels = 1;
-
 	// Correction 11/12/2016: SSAO chapter requires an SRV to the depth buffer to read from 
 	// the depth buffer.  Therefore, because we need to create two views to the same resource:
 	//   1. SRV format: DXGI_FORMAT_R24_UNORM_X8_TYPELESS
 	//   2. DSV Format: DXGI_FORMAT_D24_UNORM_S8_UINT
 	// we need to create the depth buffer resource with a typeless format.  
 	depthStencilDesc.Format = DXGI_FORMAT_R24G8_TYPELESS;
-
     depthStencilDesc.SampleDesc.Count = m4xMsaaState ? 4 : 1;
     depthStencilDesc.SampleDesc.Quality = m4xMsaaState ? (m4xMsaaQuality - 1) : 0;
     depthStencilDesc.Layout = D3D12_TEXTURE_LAYOUT_UNKNOWN;
     depthStencilDesc.Flags = D3D12_RESOURCE_FLAG_ALLOW_DEPTH_STENCIL;
-
     D3D12_CLEAR_VALUE optClear;
     optClear.Format = mDepthStencilFormat;
     optClear.DepthStencil.Depth = 1.0f;
     optClear.DepthStencil.Stencil = 0;
-    ThrowIfFailed(md3dDevice->CreateCommittedResource(
-        &CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT),
-		D3D12_HEAP_FLAG_NONE,
-        &depthStencilDesc,
-		D3D12_RESOURCE_STATE_COMMON,
-        &optClear,
-        IID_PPV_ARGS(mDepthStencilBuffer.GetAddressOf())));
-
+    ThrowIfFailed(md3dDevice->CreateCommittedResource( &CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT), D3D12_HEAP_FLAG_NONE, &depthStencilDesc, D3D12_RESOURCE_STATE_COMMON, &optClear, IID_PPV_ARGS(mDepthStencilBuffer.GetAddressOf())));
     // Create descriptor to mip level 0 of entire resource using the format of the resource.
 	D3D12_DEPTH_STENCIL_VIEW_DESC dsvDesc;
 	dsvDesc.Flags = D3D12_DSV_FLAG_NONE;
@@ -211,19 +189,15 @@ void D3DApp::OnResize()
 	dsvDesc.Format = mDepthStencilFormat;
 	dsvDesc.Texture2D.MipSlice = 0;
     md3dDevice->CreateDepthStencilView(mDepthStencilBuffer.Get(), &dsvDesc, DepthStencilView());
-
     // Transition the resource from its initial state to be used as a depth buffer.
-	mCommandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(mDepthStencilBuffer.Get(),
-		D3D12_RESOURCE_STATE_COMMON, D3D12_RESOURCE_STATE_DEPTH_WRITE));
-	
+	mCommandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(mDepthStencilBuffer.Get(), D3D12_RESOURCE_STATE_COMMON, D3D12_RESOURCE_STATE_DEPTH_WRITE));
     // Execute the resize commands.
     ThrowIfFailed(mCommandList->Close());
     ID3D12CommandList* cmdsLists[] = { mCommandList.Get() };
     mCommandQueue->ExecuteCommandLists(_countof(cmdsLists), cmdsLists);
-
 	// Wait until resize is complete.
 	FlushCommandQueue();
-
+	//9，设置视口和裁剪矩形
 	// Update the viewport transform to cover the client area.
 	mScreenViewport.TopLeftX = 0;
 	mScreenViewport.TopLeftY = 0;
@@ -231,10 +205,11 @@ void D3DApp::OnResize()
 	mScreenViewport.Height   = static_cast<float>(mClientHeight);
 	mScreenViewport.MinDepth = 0.0f;
 	mScreenViewport.MaxDepth = 1.0f;
-
     mScissorRect = { 0, 0, mClientWidth, mClientHeight };
 }
- 
+/// <summary>
+/// Done
+/// </summary>
 LRESULT D3DApp::MsgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
 	switch( msg )
@@ -254,7 +229,6 @@ LRESULT D3DApp::MsgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 			mTimer.Start();
 		}
 		return 0;
-
 	// WM_SIZE is sent when the user resizes the window.  
 	case WM_SIZE:
 		// Save the new client area dimensions.
@@ -277,7 +251,6 @@ LRESULT D3DApp::MsgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 			}
 			else if( wParam == SIZE_RESTORED )
 			{
-				
 				// Restoring from minimized state?
 				if( mMinimized )
 				{
@@ -285,7 +258,6 @@ LRESULT D3DApp::MsgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 					mMinimized = false;
 					OnResize();
 				}
-
 				// Restoring from maximized state?
 				else if( mMaximized )
 				{
@@ -311,14 +283,12 @@ LRESULT D3DApp::MsgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 			}
 		}
 		return 0;
-
 	// WM_EXITSIZEMOVE is sent when the user grabs the resize bars.
 	case WM_ENTERSIZEMOVE:
 		mAppPaused = true;
 		mResizing  = true;
 		mTimer.Stop();
 		return 0;
-
 	// WM_EXITSIZEMOVE is sent when the user releases the resize bars.
 	// Here we reset everything based on the new window dimensions.
 	case WM_EXITSIZEMOVE:
@@ -327,24 +297,20 @@ LRESULT D3DApp::MsgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 		mTimer.Start();
 		OnResize();
 		return 0;
- 
 	// WM_DESTROY is sent when the window is being destroyed.
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		return 0;
-
 	// The WM_MENUCHAR message is sent when a menu is active and the user presses 
 	// a key that does not correspond to any mnemonic or accelerator key. 
 	case WM_MENUCHAR:
         // Don't beep when we alt-enter.
         return MAKELRESULT(0, MNC_CLOSE);
-
 	// Catch this message so to prevent the window from becoming too small.
 	case WM_GETMINMAXINFO:
 		((MINMAXINFO*)lParam)->ptMinTrackSize.x = 200;
 		((MINMAXINFO*)lParam)->ptMinTrackSize.y = 200; 
 		return 0;
-
 	case WM_LBUTTONDOWN:
 	case WM_MBUTTONDOWN:
 	case WM_RBUTTONDOWN:
@@ -368,10 +334,11 @@ LRESULT D3DApp::MsgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 
         return 0;
 	}
-
 	return DefWindowProc(hwnd, msg, wParam, lParam);
 }
-
+/// <summary>
+/// Done
+/// </summary>
 bool D3DApp::InitMainWindow()
 {
 	WNDCLASS wc;
@@ -385,67 +352,50 @@ bool D3DApp::InitMainWindow()
 	wc.hbrBackground = (HBRUSH)GetStockObject(NULL_BRUSH);
 	wc.lpszMenuName  = 0;
 	wc.lpszClassName = L"MainWnd";
-
 	if( !RegisterClass(&wc) )
 	{
 		MessageBox(0, L"RegisterClass Failed.", 0, 0);
 		return false;
 	}
-
 	// Compute window rectangle dimensions based on requested client area dimensions.
 	RECT R = { 0, 0, mClientWidth, mClientHeight };
     AdjustWindowRect(&R, WS_OVERLAPPEDWINDOW, false);
 	int width  = R.right - R.left;
 	int height = R.bottom - R.top;
-
-	mhMainWnd = CreateWindow(L"MainWnd", mMainWndCaption.c_str(), 
-		WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, width, height, 0, 0, mhAppInst, 0); 
+	mhMainWnd = CreateWindow(L"MainWnd", mMainWndCaption.c_str(), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, width, height, 0, 0, mhAppInst, 0); 
 	if( !mhMainWnd )
 	{
 		MessageBox(0, L"CreateWindow Failed.", 0, 0);
 		return false;
 	}
-
 	ShowWindow(mhMainWnd, SW_SHOW);
 	UpdateWindow(mhMainWnd);
-
 	return true;
 }
-
-bool D3DApp::InitDirect3D()
-{
-#if defined(DEBUG) || defined(_DEBUG) 
-	// Enable the D3D12 debug layer.
+/// <summary>
+/// Done
+/// </summary>
+bool D3DApp::InitDirect3D(){
+#if defined(DEBUG) || defined(_DEBUG) // Enable the D3D12 debug layer.
 {
 	ComPtr<ID3D12Debug> debugController;
 	ThrowIfFailed(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController)));
 	debugController->EnableDebugLayer();
 }
 #endif
-
 	ThrowIfFailed(CreateDXGIFactory1(IID_PPV_ARGS(&mdxgiFactory)));
-
 	// Try to create hardware device.
-	HRESULT hardwareResult = D3D12CreateDevice(
-		nullptr,             // default adapter
-		D3D_FEATURE_LEVEL_11_0,
-		IID_PPV_ARGS(&md3dDevice));
-
+	//1，用D3D12CreateDevice函数创建ID3D12Device接口实例
+	HRESULT hardwareResult = D3D12CreateDevice( nullptr, D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&md3dDevice));// default adapter
 	// Fallback to WARP device.
 	if(FAILED(hardwareResult))
 	{
 		ComPtr<IDXGIAdapter> pWarpAdapter;
 		ThrowIfFailed(mdxgiFactory->EnumWarpAdapter(IID_PPV_ARGS(&pWarpAdapter)));
-
-		ThrowIfFailed(D3D12CreateDevice(
-			pWarpAdapter.Get(),
-			D3D_FEATURE_LEVEL_11_0,
-			IID_PPV_ARGS(&md3dDevice)));
+		ThrowIfFailed(D3D12CreateDevice( pWarpAdapter.Get(), D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&md3dDevice)));
 	}
-
-	ThrowIfFailed(md3dDevice->CreateFence(0, D3D12_FENCE_FLAG_NONE,
-		IID_PPV_ARGS(&mFence)));
-
+	//2，创建一个ID3D12Fence对象，并查询描述符的大小
+	ThrowIfFailed(md3dDevice->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&mFence)));
 	mRtvDescriptorSize = md3dDevice->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
 	mDsvDescriptorSize = md3dDevice->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_DSV);
 	mCbvSrvUavDescriptorSize = md3dDevice->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
@@ -453,60 +403,47 @@ bool D3DApp::InitDirect3D()
     // Check 4X MSAA quality support for our back buffer format.
     // All Direct3D 11 capable devices support 4X MSAA for all render 
     // target formats, so we only need to check quality support.
-
+	//3，检测用户设备对4X MSAA质量级别的支持情况
 	D3D12_FEATURE_DATA_MULTISAMPLE_QUALITY_LEVELS msQualityLevels;
 	msQualityLevels.Format = mBackBufferFormat;
 	msQualityLevels.SampleCount = 4;
 	msQualityLevels.Flags = D3D12_MULTISAMPLE_QUALITY_LEVELS_FLAG_NONE;
 	msQualityLevels.NumQualityLevels = 0;
-	ThrowIfFailed(md3dDevice->CheckFeatureSupport(
-		D3D12_FEATURE_MULTISAMPLE_QUALITY_LEVELS,
-		&msQualityLevels,
-		sizeof(msQualityLevels)));
-
+	ThrowIfFailed(md3dDevice->CheckFeatureSupport( D3D12_FEATURE_MULTISAMPLE_QUALITY_LEVELS, &msQualityLevels, sizeof(msQualityLevels)));
     m4xMsaaQuality = msQualityLevels.NumQualityLevels;
 	assert(m4xMsaaQuality > 0 && "Unexpected MSAA quality level.");
 	
 #ifdef _DEBUG
     LogAdapters();
 #endif
-
+	//4，依次创建命令队列、命令列表分配器和主命令列表
 	CreateCommandObjects();
+	//5，描述并创建交换链
     CreateSwapChain();
+	//6，创建应用程序所需的描述符堆
     CreateRtvAndDsvDescriptorHeaps();
-
 	return true;
 }
-
+/// <summary>
+/// 依次创建命令队列、命令列表分配器和主命令列表
+/// </summary>
 void D3DApp::CreateCommandObjects()
 {
 	D3D12_COMMAND_QUEUE_DESC queueDesc = {};
 	queueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
 	queueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
-	ThrowIfFailed(md3dDevice->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&mCommandQueue)));
-
-	ThrowIfFailed(md3dDevice->CreateCommandAllocator(
-		D3D12_COMMAND_LIST_TYPE_DIRECT,
-		IID_PPV_ARGS(mDirectCmdListAlloc.GetAddressOf())));
-
-	ThrowIfFailed(md3dDevice->CreateCommandList(
-		0,
-		D3D12_COMMAND_LIST_TYPE_DIRECT,
-		mDirectCmdListAlloc.Get(), // Associated command allocator
-		nullptr,                   // Initial PipelineStateObject
-		IID_PPV_ARGS(mCommandList.GetAddressOf())));
-
-	// Start off in a closed state.  This is because the first time we refer 
-	// to the command list we will Reset it, and it needs to be closed before
-	// calling Reset.
+	ThrowIfFailed(md3dDevice->CreateCommandQueue( &queueDesc, IID_PPV_ARGS(&mCommandQueue)));
+	ThrowIfFailed(md3dDevice->CreateCommandAllocator( D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(mDirectCmdListAlloc.GetAddressOf())));
+	ThrowIfFailed(md3dDevice->CreateCommandList( 0, D3D12_COMMAND_LIST_TYPE_DIRECT, mDirectCmdListAlloc.Get(), nullptr, IID_PPV_ARGS(mCommandList.GetAddressOf())));
 	mCommandList->Close();
 }
-
+/// <summary>
+/// 描述并创建交换链
+/// </summary>
 void D3DApp::CreateSwapChain()
 {
     // Release the previous swapchain we will be recreating.
     mSwapChain.Reset();
-
     DXGI_SWAP_CHAIN_DESC sd;
     sd.BufferDesc.Width = mClientWidth;
     sd.BufferDesc.Height = mClientHeight;
@@ -523,32 +460,26 @@ void D3DApp::CreateSwapChain()
     sd.Windowed = true;
 	sd.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
     sd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
-
 	// Note: Swap chain uses queue to perform flush.
-    ThrowIfFailed(mdxgiFactory->CreateSwapChain(
-		mCommandQueue.Get(),
-		&sd, 
-		mSwapChain.GetAddressOf()));
+    ThrowIfFailed(mdxgiFactory->CreateSwapChain( mCommandQueue.Get(), &sd, mSwapChain.GetAddressOf()));
 }
-
+/// <summary>
+/// Done
+/// </summary>
 void D3DApp::FlushCommandQueue()
 {
 	// Advance the fence value to mark commands up to this fence point.
     mCurrentFence++;
-
     // Add an instruction to the command queue to set a new fence point.  Because we 
 	// are on the GPU timeline, the new fence point won't be set until the GPU finishes
 	// processing all the commands prior to this Signal().
     ThrowIfFailed(mCommandQueue->Signal(mFence.Get(), mCurrentFence));
-
 	// Wait until the GPU has completed commands up to this fence point.
     if(mFence->GetCompletedValue() < mCurrentFence)
 	{
 		HANDLE eventHandle = CreateEventEx(nullptr, false, false, EVENT_ALL_ACCESS);
-
         // Fire event when GPU hits current fence.  
         ThrowIfFailed(mFence->SetEventOnCompletion(mCurrentFence, eventHandle));
-
         // Wait until the GPU hits current fence event is fired.
 		WaitForSingleObject(eventHandle, INFINITE);
         CloseHandle(eventHandle);
@@ -562,49 +493,41 @@ ID3D12Resource* D3DApp::CurrentBackBuffer()const
 
 D3D12_CPU_DESCRIPTOR_HANDLE D3DApp::CurrentBackBufferView()const
 {
-	return CD3DX12_CPU_DESCRIPTOR_HANDLE(
-		mRtvHeap->GetCPUDescriptorHandleForHeapStart(),
-		mCurrBackBuffer,
-		mRtvDescriptorSize);
+	return CD3DX12_CPU_DESCRIPTOR_HANDLE( mRtvHeap->GetCPUDescriptorHandleForHeapStart(), mCurrBackBuffer, mRtvDescriptorSize);
 }
 
 D3D12_CPU_DESCRIPTOR_HANDLE D3DApp::DepthStencilView()const
 {
 	return mDsvHeap->GetCPUDescriptorHandleForHeapStart();
 }
-
+/// <summary>
+/// Done
+/// </summary>
 void D3DApp::CalculateFrameStats()
 {
 	// Code computes the average frames per second, and also the 
 	// average time it takes to render one frame.  These stats 
 	// are appended to the window caption bar.
-    
 	static int frameCnt = 0;
 	static float timeElapsed = 0.0f;
-
 	frameCnt++;
-
 	// Compute averages over one second period.
 	if( (mTimer.TotalTime() - timeElapsed) >= 1.0f )
 	{
 		float fps = (float)frameCnt; // fps = frameCnt / 1
 		float mspf = 1000.0f / fps;
-
         wstring fpsStr = to_wstring(fps);
         wstring mspfStr = to_wstring(mspf);
-
-        wstring windowText = mMainWndCaption +
-            L"    fps: " + fpsStr +
-            L"   mspf: " + mspfStr;
-
+        wstring windowText = mMainWndCaption + L"    fps: " + fpsStr + L"   mspf: " + mspfStr;
         SetWindowText(mhMainWnd, windowText.c_str());
-		
 		// Reset for next average.
 		frameCnt = 0;
 		timeElapsed += 1.0f;
 	}
 }
-
+/// <summary>
+/// Done
+/// </summary>
 void D3DApp::LogAdapters()
 {
     UINT i = 0;
@@ -614,25 +537,22 @@ void D3DApp::LogAdapters()
     {
         DXGI_ADAPTER_DESC desc;
         adapter->GetDesc(&desc);
-
         std::wstring text = L"***Adapter: ";
         text += desc.Description;
         text += L"\n";
-
         OutputDebugString(text.c_str());
-
         adapterList.push_back(adapter);
-        
         ++i;
     }
-
     for(size_t i = 0; i < adapterList.size(); ++i)
     {
         LogAdapterOutputs(adapterList[i]);
         ReleaseCom(adapterList[i]);
     }
 }
-
+/// <summary>
+/// Done
+/// </summary>
 void D3DApp::LogAdapterOutputs(IDXGIAdapter* adapter)
 {
     UINT i = 0;
@@ -641,41 +561,31 @@ void D3DApp::LogAdapterOutputs(IDXGIAdapter* adapter)
     {
         DXGI_OUTPUT_DESC desc;
         output->GetDesc(&desc);
-        
         std::wstring text = L"***Output: ";
         text += desc.DeviceName;
         text += L"\n";
         OutputDebugString(text.c_str());
-
         LogOutputDisplayModes(output, mBackBufferFormat);
-
         ReleaseCom(output);
-
         ++i;
     }
 }
-
+/// <summary>
+/// Done
+/// </summary>
 void D3DApp::LogOutputDisplayModes(IDXGIOutput* output, DXGI_FORMAT format)
 {
     UINT count = 0;
     UINT flags = 0;
-
     // Call with nullptr to get list count.
     output->GetDisplayModeList(format, flags, &count, nullptr);
-
     std::vector<DXGI_MODE_DESC> modeList(count);
     output->GetDisplayModeList(format, flags, &count, &modeList[0]);
-
     for(auto& x : modeList)
     {
         UINT n = x.RefreshRate.Numerator;
         UINT d = x.RefreshRate.Denominator;
-        std::wstring text =
-            L"Width = " + std::to_wstring(x.Width) + L" " +
-            L"Height = " + std::to_wstring(x.Height) + L" " +
-            L"Refresh = " + std::to_wstring(n) + L"/" + std::to_wstring(d) +
-            L"\n";
-
+        std::wstring text = L"Width = " + std::to_wstring(x.Width) + L" " + L"Height = " + std::to_wstring(x.Height) + L" " + L"Refresh = " + std::to_wstring(n) + L"/" + std::to_wstring(d) + L"\n";
         ::OutputDebugString(text.c_str());
     }
 }
